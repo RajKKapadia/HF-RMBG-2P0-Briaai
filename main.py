fb3a5ef1a676da62908de40355682b984027795d
RajKKapadia <raajforyou@gmail.com>
2026-02-04 10:18:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import warnings
+
+from PIL import Image
+import torch
+from torchvision import transforms
+from transformers import AutoModelForImageSegmentation
+
+# Ingnore warnings
+warnings.filterwarnings("ignore")
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Using device: {device}")
+model = AutoModelForImageSegmentation.from_pretrained('briaai/RMBG-2.0', trust_remote_code=True)
+model.eval().to(device)
+
+# Data settings
+image_size = (1024, 1024)
+transform_image = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+input_image_path = "test_3.jpg"
+
+image = Image.open(input_image_path)
+input_images = transform_image(image).unsqueeze(0).to(device)
+
+# Prediction
+with torch.no_grad():
+    preds = model(input_images)[-1].sigmoid().cpu()
+pred = preds[0].squeeze()
+pred_pil = transforms.ToPILImage()(pred)
+mask = pred_pil.resize(image.size)
+image.putalpha(mask)
+
+image.save("no_bg_image.png")
